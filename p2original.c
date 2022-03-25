aea7b4881ce319fcaee9fc7cf1a376f13b0bd6e5
Sruthi <93090631+ChinthimiSruthi@users.noreply.github.com>
2022-03-25 08:37:24 +0000

    p2 final

--- p2original.c
+++ p2original.c
@@ -14,13 +14,13 @@ int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
 {
   int a,b,c;
-  if((a+b)>c && (b+c)>a && (c+a)>b)
+  if((a+b)>c || (b+c)>a || (c+a)>b)
   {
     printf("Three lines form a triangle\n");
   }
   else
   {
-    printf("Three lines doesnot form a triangle\n);
+    printf("Three lines doesnot form a triangle\n");
   }
 }
 int main()
@@ -28,6 +28,7 @@ int main()
   float x1,y1,x2,y2,x3,y3;
   input_line(&x1,&y1);
   is_triangle(x1,y1,x2,y2,x3,y3);
-  output(x1,y
+  int istriangle;
+  output(x1,y1,x2,y2,x3,y3,istriangle);
   return 0;
 }
--- p5original.c
+++ p5original.c
@@ -1,29 +1,59 @@
 #include <stdio.h>
-int input_array_size()
+#include <math.h>
+int input()
 {
-  int a;
-  printf("Enter the size of array\n");
-  scanf("%d",&a);
-  return a;
+  int n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
 }
-void input_array(int n, int a[n]
+
+void input_array(int n, int a[n])
 {
-  int i;
-  printf("Enter a number\n");
-  for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
+  for(int i=0;i<n;i++)
+    a[i] = i;
+  a[1]=0;
 }
+
 void erotosthenes_sieve(int n, int a[n])
 {
-  int i,j,flag=0;
-  for(i=1;i<=n;i++)
-    {
-      for(j=1;j<=n;j++)
+  int i=1,j,count;
+  while(i<=n)
+  {
+   count=0;
+    i=n;
+    while(j<=i/2)
+      {
+        if(i%j==0)
         {
-          if(i%j==0)
-          {
-            flag++;
-          }
+          count++;
+        break;
         }
+        j++;
+      }
+    if(count==0 && i!=n)
+    {
+      printf("%d\n",i);
     }
+    i++;
+  }
+}
+
+void out_put(int n, int a[n])
+{
+  for(int i=0;i<n;i++)
+    if(a[i]!=0)
+      printf("%d ",a[i]);
+  printf("\n");
+}
+
+int main()
+{
+  int n;
+  n=input();
+  int a[n];
+  input_array(n,a);
+  erotosthenes_sieve(n,a);
+  out_put(n,a);
+  return 0;
 }
